77fc72f80e24563ed4b733944bacb8fc511a35c2
7kia <il7kia@yandex.ru>
2017-02-06 15:27:36 +0300

    Add part first task

---
+++ Kolchin1Task.hs
@@ -0,0 +1,19 @@
+v1 = (( 1, "a"), "abc")
+snd (fst v1)
+
+v21 = ["a", "b", "c"]
+head (tail v21)
+
+v22 = [["a", "b"], ["c","d"]]
+tail(head v22)
+
+v23 = [["a", "c", "d"], ["a","b"]]
+tail(head (tail v23))
+
+v24 = [["a","d"], ["b", "c"]]
+head (head (tail v24))
+
+v31 = [x*2 + 1 | x <- [0..19]]
+v32 = [ x | x <- [1..40], x `mod` 2 == 1]
+v33 = [ x | x <- [1..40], odd x ]
+
