0ebc9628a8080fdf1edfb0c97b0120d3e2c6abb4
7kia <il7kia@yandex.ru>
2017-02-22 23:22:18 +0300

    Something function rewrite, can accept difference types

--- Kolchin3Task.hs
+++ Kolchin3Task.hs
@@ -1,4 +1,4 @@
-module Lab2(listnums, listnums2, secondlastlist) where
+module Lab3(listnums, secondlastlist, myunion, mysubst, fiveFunc) where
 {- 1 task -}
 listnums :: Int->[Int]
 listnums 0 = []
@@ -8,41 +8,40 @@ secondlastlist :: [[a]]->[a]
 secondlastlist [] = []
 secondlastlist (x:xs) = last x : secondlastlist xs
 {- 3 task -}
--- TODO : concre type
-isInList1 :: Int->[Int]->Bool
+isInList1 :: Eq a => a->[a]->Bool
 isInList1 a (x:xs) = if a == x then True
 else isInList1 a xs
 isInList1 a [] = False
 
-isInList2 :: [Int]->[Int]->[Bool]
+isInList2 :: Eq a => [a]->[a]->[Bool]
 isInList2 [] _ = []
 isInList2 (a:as) (x:xs) = isInList1 a (x:xs) : isInList2 as (x:xs)
 
-extractElement :: (Bool, Int)->[Int]
+extractElement :: Eq a => (Bool, a)->[a]
 extractElement (contain, number) = if contain == True 
 then [number]
 else []
 
-extractElements :: [(Bool, Int)]->[Int]
+extractElements :: Eq a => [(Bool, a)]->[a]
 extractElements [] = []
 extractElements (x:xs) = extractElement x ++ extractElements xs
 
-myunion :: [Int]->[Int]->[Int]
+myunion :: Eq a => [a]->[a]->[a]
 myunion [] _ = []
 myunion (x:xs) (y:ys) = extractElements (zip (isInList2 (x:xs) (y:ys)) (x:xs))
 {- 4 task -}
-extractElement2 :: (Bool, Int)->[Int]
+extractElement2 :: Eq a => (Bool, a)->[a]
 extractElement2 (contain, number) = if contain == False 
 then [number]
 else []
 
-extractElements2 :: [(Bool, Int)]->[Int]
+extractElements2 :: Eq a => [(Bool, a)]->[a]
 extractElements2 [] = []
 extractElements2 (x:xs) = extractElement2 x ++ extractElements2 xs
 
-mysubst :: [Int]->[Int]->[Int]
+mysubst :: Eq a => [a]->[a]->[a]
 mysubst [] _ = []
 mysubst (x:xs) (y:ys) = extractElements2 (zip (isInList2 (x:xs) (y:ys)) (x:xs))
 {- 5 task -}
-fiveFunc :: [[Int]]->Int->[Int]
+fiveFunc :: [[a]]->Int->[a]
 fiveFunc (x:xs) pos = map (!! pos) (x:xs)
