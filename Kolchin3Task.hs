1fc184f9b2a0aad7a74b3f19bdfc33e8b8630d20
7kia <il7kia@yandex.ru>
2017-02-22 08:44:09 +0300

    Add 2 task and part 3

--- Kolchin3Task.hs
+++ Kolchin3Task.hs
@@ -1,4 +1,4 @@
-module Lab2(listnums, listnums2) where
+module Lab2(listnums, listnums2, secondlastlist ) where
 {- 1 task -}
 listnums :: Int->[Int]
 listnums 0 = []
@@ -6,4 +6,19 @@ listnums n = n : listnums(n-1)
 {- not recursive -}
 listnums2 :: Int->[Int]
 listnums2 n = reverse [1..n]
-{- 1 task -}
+{- 2 task -}
+secondlastlist :: [[a]]->[a]
+secondlastlist [] = []
+secondlastlist (x:xs) = last x : secondlastlist xs
+{- 3 task -}
+-- TODO : concre type
+isInList1 :: Int->[Int]->Bool
+isInList1 a (x:xs) = if a == x then True
+else isInList1 a xs
+isInList1 a [] = False
+
+myunion :: [Int]->[Int]->[Bool]
+myunion (x:xs) (y:ys) = isInList1 x (y:ys) : []
+-- TODO ^^^
+--[1,2,3][4,5,1]
+--[1,2,3][1][4,5,1]
