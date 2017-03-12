a0f70cd6cbb1a94506a1ce728d41236065e52911
7kia <il7kia@yandex.ru>
2017-03-12 13:26:30 +0300

    Add second task 5 laboratory

---
+++ Kolchin5Task-2.hs
@@ -0,0 +1,25 @@
+﻿import System.IO
+import Data.Char ( toUpper )
+import System.Environment 
+  
+copyFile :: Handle -> Handle -> String -> IO ()
+copyFile h1 h2 symbol = do
+                   eof <- hIsEOF h1
+                   if eof then return () else
+                      do
+                        c <- hGetChar h1
+                        hPutChar h2 (replace c symbol)   
+                        copyFile h1 h2 symbol
+
+main = do 
+         [f1,f2, symbol] <- getArgs
+         h1 <- openFile f1 ReadMode     
+         h2 <- openFile f2 WriteMode 
+         copyFile h1 h2 symbol           
+         hClose h1                  
+         hClose h2
+
+replace :: Char->String->Char
+replace replaceSym symbol = if replaceSym `elem` [',' , ';'] 
+then head symbol 
+else replaceSym 
