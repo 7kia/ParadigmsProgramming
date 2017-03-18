4b9775e2e864a7427fd9d86f3094b7824c852eda
7kia <il7kia@yandex.ru>
2017-03-18 18:34:16 +0300

    Add first task for fifth laboratory

---
+++ Kolchin5Task-1.hs
@@ -0,0 +1,13 @@
+﻿getMultiplicityElements :: Int -> Int -> Int -> [Int]
+getMultiplicityElements startValue amount multiplicity = [ x | x <- [startValue..(startValue + (amount - 1) * multiplicity)],
+ ((x `mod` multiplicity) == 0) ]
+
+main = do
+putStrLn "Введите начальное значение, количество элементов, кратность."
+putStrLn "Начальное значение = "
+startValue <- getLine
+putStrLn "Количество = "
+amount <- getLine
+putStrLn "Кратность ="
+multiplicity <- getLine
+print (getMultiplicityElements (read startValue) (read amount) (read multiplicity))
