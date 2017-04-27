9e8442d2def9954e3be300a597f4c3286abe6246
7kia <il7kia@yandex.ru>
2017-04-27 15:39:47 +0300

    Rewrite 2 lab, done 3

--- Prolog/lab2-1.pl
+++ Prolog/lab2-1.pl
@@ -8,68 +8,27 @@ seg(7,point(10,3),point(10,12)).
 seg(8,point(11,3),point(11,13)).
 seg(9,point(12,2),point(12,12)).
 
-horiz(1).
-horiz(2).
-horiz(3).
-horiz(5).
+vertical(N) :- seg(N, point(X1, _), point(X2, _)), X1 = X2.
+horiz(N) :- seg(N, point(_, Y1), point(_, Y2)), Y1 = Y2.
 
-vertical(4).
-vertical(6).
-vertical(7).
-vertical(8).
-vertical(9).
+cross(LINE1,LINE2,point(X,Y),LINE1LEN,LINE2LEN):-
+	seg(LINE1, point(X3, Y3), point(X4, Y4)),
+    seg(LINE2, point(X1, Y1), point(X2, _)),
+    Y3 =< Y1, Y1 =< Y4, 
+    X1 =< X3, X3 =< X2,
+    X is X3, Y is Y1,
+	LINE1 \= LINE2,
+    LINE1LEN is Y4 - Y3,
+    LINE2LEN is X2 - X1.
 
-perpendicular(X,Y):-horiz(X),vertical(Y).
-perpendicular(X,Y):-horiz(Y),vertical(X).
-
-someCoordinate(N,COOR):-horiz(N),seg(N,point(_,COOR),_).
-someCoordinate(N,COOR):-vertical(N),seg(N,point(COOR,_),_).
-firstCoordinate(N,COOR):-horiz(N),seg(N,point(COOR,_),_).
-firstCoordinate(N,COOR):-vertical(N),seg(N,point(_,COOR),_).
-secondCoordinate(N,COOR):-horiz(N),seg(N,_,point(COOR,_)).
-secondCoordinate(N,COOR):-vertical(N),seg(N,_,point(_,COOR)).
-
-xForFirst(N,COOR):-horiz(N),seg(N,point(COOR,_),_).
-xForFirst(N,COOR):-vertical(N),seg(N,point(COOR,_),_).
-yForFirst(N,COOR):-horiz(N),seg(N,point(_,COOR),_).
-yForFirst(N,COOR):-vertical(N),seg(N,point(_,COOR),_).
-xForSecond(N,COOR):-horiz(N),seg(N,_,point(COOR,_)).
-xForSecond(N,COOR):-vertical(N),seg(N,_,point(COOR,_)).
-yForSecond(N,COOR):-horiz(N),seg(N,_,point(_,COOR)).
-yForSecond(N,COOR):-vertical(N),seg(N,_,point(_,COOR)).
-
-interpoint(LINE1,LINE2,point(X,Y)):-horiz(LINE1),vertical(LINE2),someCoordinate(LINE1,Y),someCoordinate(LINE2,X).
-interpoint(LINE1,LINE2,point(X,Y)):-horiz(LINE2),vertical(LINE1),someCoordinate(LINE1,X),someCoordinate(LINE2,Y).
-len(X,LENGTH):-xForFirst(X,XFOR1),xForSecond(X,XFOR2),
-yForFirst(X,YFOR1),yForSecond(X,YFOR2),LENGTH is sqrt((XFOR1 - XFOR2)^2 + (YFOR1 - YFOR2)^2).
-
-crossimpl(LINE1,LINE2,point(X,Y)):-horiz(LINE1),
-vertical(LINE2),
-interpoint(LINE1,LINE2,point(X,Y)),
-firstCoordinate(LINE1,NMI),secondCoordinate(LINE1,NMA),
-firstCoordinate(LINE2,MMI),secondCoordinate(LINE2,MMA),
-NMI=<X,
-X=<NMA,
-MMI=<Y,
-Y=<MMA.
-
-crossimpl(LINE1,LINE2,point(X,Y)):-vertical(LINE1),
-horiz(LINE2),
-crossimpl(LINE2,LINE1,point(X,Y)).
-
-cross(LINE1,LINE2,point(X,Y),LINE1LEN,LINE2LEN):-crossimpl(LINE1,LINE2,point(X,Y)),len(LINE1,LINE1LEN),len(LINE2,LINE2LEN).
-
-per_sq_impl(N1,N2,N3,N4,P,S):-N1\=N3,
-N2\=N4,
-cross(N1,N2,point(N1N2X,N1N2Y),_,_),
-cross(N2,N3,_,_,_),
-cross(N3,N4,point(N3N4X,N3N4Y),_,_),
-cross(N4,N1,_,_,_),
-N4X is abs(N1N2X-N3N4X),
-N4Y is abs(N1N2Y-N3N4Y),
-P is 2*(N4X+N4Y),
-S is N4X*N4Y.
-
-perimetr(N1,N2,N3,N4,P,S):-per_sq_impl(N1,N2,N3,N4,P,S).
-perimetr(N1,N2,N3,N4,P,S):-per_sq_impl(N1,N2,N4,N3,P,S).
-perimetr(N1,N2,N3,N4,P,S):-per_sq_impl(N1,N3,N2,N4,P,S).
+perimetr(N1, N2, N3, N4, P, S) :-
+    cross(N1, N2, point(X1, Y1), _, _),
+    cross(N1, N4, point(_, _), _, _),
+    cross(N3, N2, point(_, _), _, _),
+    cross(N3, N4, point(X2, Y2), _, _),
+    P is ((Y2 - Y1) + (X2 - X1)) * 2,
+    S is ((Y2 - Y1) * (X2 - X1)),
+	Y2 > Y1, X2 > X1,
+    N1 \= N2, N1 \= N3, N1 \= N4,
+    N2 \= N3, N2 \= N4,
+    N3 \= N4.
---
+++ Prolog/lab3.pl
@@ -0,0 +1,103 @@
+getTail([_|Tail], L) :-
+    append(Tail, [], L).
+    
+trim(Source, Result) :-
+length(Source, Length),
+(
+	(
+		Length > 2,
+		getTail(Source, ListWithoutHead),
+		reverse(ListWithoutHead, ReverseList),
+		getTail(ReverseList, ReverseListWithoutHead),
+		reverse(ReverseListWithoutHead, Result)
+	);
+	(
+		Length < 3, append([],[], Result)
+	)
+).
+
+first([First|_], Result) :- Result = First.
+
+first_last(Source, Result) :-
+	length(Source, Length),
+	(
+		(
+			Length > 1,
+			first(Source, Head),
+			last(Source, Last),
+			trim(Source, Middle),
+			append([], [Last], L1),
+			append(L1, Middle, L2),
+			append(L2, [Head], Result)
+		);
+		(
+			Length < 2,
+			append(Source, [], Result)
+		)    
+	).
+
+total([], X) :- X = 0,!.
+total([Head], X) :- X = Head,!.
+total([Head|Tail], X) :- total(Tail, Y), X is Head * Y,!.
+
+place(X, [], L) :- append([], [X], L).
+place(X,[Head|Tail],L) :-
+	append([X], [Head|Tail], L);
+	(
+		place(X, Tail, L1),
+		append([Head], L1, L)
+	).
+
+double([], Result) :- append([], [], Result), !.
+double([Head], Result) :- Result is [Head, Head], !.
+double([Head|Tail], Result) :- 
+    double([Head], HeadList),
+    double(Tail, TailList),
+    append(HeadList, TailList, Result),
+    !.
+
+splitOdd([], L) :- append([],[],L).    
+splitOdd([Head|Tail], L) :-
+    splitOdd(Tail, L1), Mod is Head mod 2,
+    (
+        (
+            Mod == 1, append([Head], L1, L)
+        );
+        (
+            Mod == 0, append([], L1, L)
+        )
+    ).
+
+splitEven([], L) :- append([],[],L).    
+splitEven([Head|Tail], L) :-
+    splitEven(Tail, L1), Mod is Head mod 2,
+    (
+        (
+            Mod == 0, append([Head], L1, L)
+        );
+        (
+            Mod == 1, append([], L1, L)
+        )
+    ).    
+
+splitImpl(L, EvenList, OddList) :-
+		splitEven(L, EvenList),
+        splitOdd(L, OddList).
+ 
+split(L, EvenList, OddList) :-
+    reverse(L, ReverseList), splitImpl(ReverseList, EvenList, OddList).
+
+repeat3(Source, Result) :-
+    trim(Source, Trimmed),
+    first(Source, Head),
+    last(Source, Last),
+    append([], [Last, Last, Last], NewHead),
+    append(NewHead, Trimmed, L1),
+    append(L1, [Head, Head, Head], Result).
+    
+combi([], [], Result) :- append([], [], Result).
+combi([], [Head|Tail], Result) :- append([], [Head|Tail], Result).
+combi([Head|Tail], [], Result) :- append([], [Head|Tail], Result).
+combi([Head1|Tail1], [Head2|Tail2], Result) :-
+    combi(Tail1, Tail2, L1),
+    append([Head1, Head2], L1, Result).
